d01f5b214dd0dd56617f0d4752e44c86f389a284
anthonycroft <tony@vbasolutions.co.uk>
2024-10-24 20:58:04 +0100

    installed Scipy and added basic data validation.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
 
+import numpy as np
 import pandas as pd
 import requests
+from pandas import DataFrame
+from scipy import stats
 
 # calculate yesterday's date
 yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
@@ -11,7 +14,7 @@ BMRS_API_ENDPOINT = f"https://data.elexon.co.uk/bmrs/api/v1/balancing/settlement
 
 
 # Function to get system imbalance cost and price
-def get_system_imbalance_data():
+def get_system_imbalance_data() -> DataFrame:
     # Calculate previous day (BMRS typically updates for previous day)
 
     # Make the GET request to retrieve the imbalance data
@@ -34,9 +37,6 @@ def get_system_imbalance_data():
             ]
         ]
 
-        # Print the DataFrame for review
-        print(df)
-
         # Return the DataFrame for further use
         return df
 
@@ -52,6 +52,50 @@ def get_system_imbalance_data():
     else:
         print(f"Error: Unable to fetch data, Status code {response.status_code}")
 
+    # Return None if request was unsuccessful
+    return None
+
+
+def clean_data(df: DataFrame) -> DataFrame:
+    # Check for 48 rows:
+
+    df.isnull().sum()  # Check for missing values
+    df.dropna()  # Drop rows with missing values
+
+    # Convert columns to appropriate data types
+    df["settlementDate"] = pd.to_datetime(df["settlementDate"])
+    df["startTime"] = pd.to_datetime(df["startTime"])
+
+    # Filter out rows with negative values
+    df = df[df["systemSellPrice"] >= 0]
+    df = df[df["systemBuyPrice"] >= 0]
+
+    # Basic method to check for outliers using z-score
+
+    df = df[
+        (
+            np.abs(
+                stats.zscore(
+                    df[["systemSellPrice", "systemBuyPrice", "netImbalanceVolume"]]
+                )
+            )
+            < 3
+        ).all(axis=1)
+    ]
+
+    # Remove duplicate rows
+    df.drop_duplicates(inplace=True)
+
+    # Check the number of rows in the DataFrame
+    row_count = df.shape[0]
+    print(f"Number of rows: {row_count}")
+
+    # df[['systemSellPrice', 'systemBuyPrice', 'netImbalanceVolume']] = df[['systemSellPrice', 'systemBuyPrice', 'netImbalanceVolume']].fillna(0)
+
 
 if __name__ == "__main__":
-    get_system_imbalance_data()
+    df = get_system_imbalance_data()
+    clean_data(df)
+
+    # Print the DataFrame for review
+    print(df)
